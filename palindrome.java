c94c59d30994ea5657e8077c4021a3acfd93248f
DIXITH46 <deekshithasdeekshi22@gmail.com>
2026-05-05 14:38:13 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int original = x;
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x = x / 10;
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) return false;
+            reversed = reversed * 10 + digit;
+        }
+        return original == reversed;
+    }
+}
