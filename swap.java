5196fc95caafa1a65ad8432097697a80402461b6
DIXITH46 <deekshithasdeekshi22@gmail.com>
2026-05-06 10:04:41 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,34 @@
+class Main {
+    public static void main(String[] args) {
+      int a=5,b=3;
+      int temp;
+         System.out.println("a="+a+"and b="+b);
+      temp=a;
+      a=b;
+      b=temp;
+        System.out.println("a="+a+"and b="+b);
+    }
+}
+
+  
+class Main {
+    public static void main(String[] args) {
+        int a=5,b=3;
+         System.out.println("a="+a+"and b="+b);
+       a=a+b;
+       b=a-b;
+       a=a-b;
+        System.out.println("a="+a+"and b="+b);
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        int a=5,b=3;
+         System.out.println("a="+a+"and b="+b);
+       a=a^b;
+       b=a^b;
+       a=a^b;
+        System.out.println("a="+a+"and b="+b);
+    }
+}
